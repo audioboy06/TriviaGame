0aaea3e04818034b4d6fa04547a27b6b288a76e4
audioboy06 <rickjoice@Ricks-Mac-mini.local>
2017-11-28 21:13:05 -0500

    still work in progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+// QUESTIONS //
+
+var question1 = {
+    question: "On what planet is the first key hidden?",
+        a:["A: Incipio", false],
+        b:["B: Middletown", false],
+        c:["C: Ludus", true],
+        d:["D: Syrinx", false],
+};
+
+var question2 = {
+    question: "What is the name of the giant robot that Parzival chose as a prize for playing a perfect game of Pac-Man?",
+        a:["A: Ultra Man", false],
+        b:["B: MechaGodzilla", false],
+        c:["C: Voltron", false],
+        d:["D: Leopardon", true],
+};
+
+var question3 = {
+    question: "What was James Hallidays favorite band?",
+        a:["A: Black Sabbath", false],
+        b:["B: The Rolling Stones", false],
+        c:["C: Rush", true],
+        d:["D: Deep Purple", false],
+};
+
+var question4 = {
+    question: "OASIS users are able to run 'System Agent Software', who is Parzival's software modeled after?",
+        a:["A: KITT", false],
+        b:["B: HAL 9000", false],
+        c:["C: Max Headroom", true],
+        d:["D: Majel Barrett", false],
+};
+
+var question5 = {
+    question: "What is AECH's real name?",
+        a:["A: Helen", true],
+        b:["B: Henry", false],
+        c:["C: Hogan", false],
+        d:["D: Hubert", false],
+};
+
+var question6 = {
+    question: "What company owns and operates the OASIS?",
+        a:["A: IOI", false],
+        b:["B: GSS", true],
+        c:["C: AboundSound", false],
+        d:["D: The Plug", false],
+};
+
+// COUNTERS //
+
+var questionArray = [question1, question2, question3, question4, question5, question6];
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var currentQuestion = 0;
+
+// LOGIC //
+
+$(currentQuestion = 0, currentQuestion <= questionArray, currentQuestion++) {
+
+}
